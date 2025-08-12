4fd4773192e4af05a1d8a28849b3bcfd2e8d9740
matscoder <levin.hsu@outlook.com>
2025-08-12 19:11:27 +0800

    25.8.12

--- 25.8.10-Day1/hello.py
+++ 25.8.10-Day1/hello.py
@@ -1,1 +1,14 @@
+'''
+Author: matscoder levin.hsu@outlook.com
+Date: 2025-08-10 12:47:14
+LastEditors: matscoder levin.hsu@outlook.com
+LastEditTime: 2025-08-12 19:08:38
+FilePath: /Python-beginner/25.8.10-Day1/hello.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+#老韩说明
+#1.python文件的后缀是.py
+#2.print语句的作业是输出内容
+#3.输出的内容就是（）内写的内容Hello,world
+#4.在python中#表示注释，即对代码的说明，但是不会被执行
 print ("hello, world")
---
+++ 25.8.12-Day2-begin in 009/Mats.py
@@ -0,0 +1,9 @@
+'''
+Author: matscoder levin.hsu@outlook.com
+Date: 2025-08-12 14:18:54
+LastEditors: matscoder levin.hsu@outlook.com
+LastEditTime: 2025-08-12 19:08:30
+FilePath: /Python-beginner/25.8.12-Day2-begin in 009/Mats.py
+'''
+print ("Mats is studying python!")
+
