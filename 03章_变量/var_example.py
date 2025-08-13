4001bd36134fc3208a82095c0648e14e9ed96e8d
matscoder <levin.hsu@outlook.com>
2025-08-13 19:51:02 +0800

    8.13

---
+++ 03章_变量/var_example.py
@@ -0,0 +1,23 @@
+'''
+Author: matscoder levin.hsu@outlook.com
+Date: 2025-08-13 18:51:40
+LastEditors: matscoder levin.hsu@outlook.com
+LastEditTime: 2025-08-13 19:08:51
+FilePath: /Python-beginner/03章_变量/var_example.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
+
+#变量的实例
+
+# 老师解读
+#定义了一个变量，变量的名称a，变量的值是1，1是int类型(整数类型)
+a=1
+#定义了一个变量，变量的名称b，变量的值是2，2是int类型(整数类型)
+b=2
+#变量b的值修改成8,变量b的值是8,8是int类型
+b=8
+
+#输出变量的值，type(a) 表示输出 a 的类型
+print("a的值是", a, "\t类型是",type(a))
+print("b的值是", b, "\t类型是",type(b))
