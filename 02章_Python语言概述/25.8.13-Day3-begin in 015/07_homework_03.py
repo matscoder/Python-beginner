f623694dbc85fdf477da7252ebab2eb714e24537
matscoder <levin.hsu@outlook.com>
2025-08-13 19:53:17 +0800

    8.13

--- 02章_Python语言概述/25.8.13-Day3-begin in 015/07_homework_03.py
+++ 02章_Python语言概述/25.8.13-Day3-begin in 015/07_homework_03.py
@@ -1,3 +1,12 @@
+'''
+Author: matscoder levin.hsu@outlook.com
+Date: 2025-08-13 17:24:05
+LastEditors: matscoder levin.hsu@outlook.com
+LastEditTime: 2025-08-13 19:53:03
+FilePath: /Python-beginner/02章_Python语言概述/25.8.13-Day3-begin in 015/07_homework_03.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
 环境变量path配置及其作用是什么？
 1.配置环境变量path的步骤 电脑->属性->高级系统设置->环境变量————参考老师演示
 2.配置环境变量后，我们可以在任意目录去执行path指定的目录下的程序或者指令，比如python.exe
