3af7c7ec1bce40a6314cce0d6e49c5539df462eb
matscoder <levin.hsu@outlook.com>
2025-08-13 19:52:36 +0800

    8.13

--- 02章_Python语言概述/25.8.13-Day3-begin in 015/08_homework_04.py
+++ 02章_Python语言概述/25.8.13-Day3-begin in 015/08_homework_04.py
@@ -1,3 +1,12 @@
+'''
+Author: matscoder levin.hsu@outlook.com
+Date: 2025-08-13 17:24:45
+LastEditors: matscoder levin.hsu@outlook.com
+LastEditTime: 2025-08-13 19:52:21
+FilePath: /Python-beginner/02章_Python语言概述/25.8.13-Day3-begin in 015/08_homework_04.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
 Python编写有哪些规范？
 1.正确的使用注释
 2.使用正确的缩进
